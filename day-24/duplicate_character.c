7f5b826e3e2c3a7af8a2549f8e9d6c39b9b21e5a
tanya gautam <tanyagautam144@gmail.com>
2026-06-26 00:12:28 +0530

    added day-24 assignment

---
+++ day-24/duplicate_character.c
@@ -0,0 +1,38 @@
+//Write a program to Remove duplicate characters.
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, j;
+
+    // Input the string
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Compare each character with the remaining characters
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        // Skip spaces
+        if(str[i] == ' ')
+        {
+            continue;
+        }
+
+        for(j = i + 1; str[j] != '\0'; j++)
+        {
+            // If duplicate character is found
+            if(str[i] == str[j])
+            {
+                printf("%c ", str[i]);
+
+                // Mark the duplicate character so it is not printed again
+                str[j] = '*';
+
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
